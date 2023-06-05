7b7dca24e850531c99bc4ff8542fbce9e8ec0c6a
Kaina Cockett <kainacockett@gmail.com>
2023-06-05 10:31:22 -0600

    test: Biker #personal_record

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -36,4 +36,15 @@ describe 'Biker' do
       expect(@biker.rides[@ride2]).to eq([60.9, 61.6])
     end
   end
+  describe '#personal_record' do
+    it 'should return the fastest time for a given ride' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(biker.personal_record(ride1)).to eq(91.1)
+      expect(biker.personal_record(ride2)).to eq(60.9)
+    end
+  end
 end

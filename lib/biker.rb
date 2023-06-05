e2574450698bda44704219a9723e6b09b920f0a7
Kaina Cockett <kainacockett@gmail.com>
2023-06-05 09:40:24 -0600

    feat: Biker

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,1 +1,12 @@
 require './lib/ride'
+
+class Biker
+  attr_reader :name, :max_distance, :rides, :acceptable_terrain
+
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
+  end
+end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,10 +4,10 @@ require './lib/biker'
 
 describe 'Biker' do
   before(:each) do
-    biker = Biker.new("Kenny", 30)
-    biker2 = Biker.new("Athena", 15)
-    ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
   describe '#initialize' do
     it 'should initialize' do
